fe8a3dfa828e859191bc9783a38a11390acdb9ac
AA-ron <aaron.henderson@rfa.sc.gov>
2019-03-22 09:31:55 -0600

    hide system bar

--- src/background.js
+++ src/background.js
@@ -15,8 +15,7 @@ let win;
 protocol.registerStandardSchemes(["app"], { secure: true });
 function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({ width: 600, height: 600 });
-
+  win = new BrowserWindow({ width: 600, height: 600, frame: false });
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
     win.loadURL(process.env.WEBPACK_DEV_SERVER_URL);
